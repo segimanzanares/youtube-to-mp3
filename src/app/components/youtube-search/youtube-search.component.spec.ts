92fefbe8ba4469b6d72988f29181f8f48057a188
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2023-03-22 14:34:50 -0600

    Create youtube search component

---
+++ src/app/components/youtube-search/youtube-search.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { YoutubeSearchComponent } from './youtube-search.component';
+
+describe('YoutubeSearchComponent', () => {
+  let component: YoutubeSearchComponent;
+  let fixture: ComponentFixture<YoutubeSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ YoutubeSearchComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(YoutubeSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
