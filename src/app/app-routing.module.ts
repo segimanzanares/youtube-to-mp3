734096085d1c6ede0f6551f1fbb83d9cb0555750
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2023-03-22 10:53:56 -0600

    Create home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        data: { title: 'Home', name: 'home' }
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
