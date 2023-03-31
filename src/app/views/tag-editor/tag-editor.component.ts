1d8308a9d25ff09419544deeaf8ba779c958760c
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2023-03-31 15:47:24 -0600

    Display audio directory

--- src/app/views/tag-editor/tag-editor.component.ts
+++ src/app/views/tag-editor/tag-editor.component.ts
@@ -17,7 +17,7 @@ export class TagEditorComponent {
         this.ipcService.readAudioTagsFromFolder().then(response => {
             console.log(response)
             if (response) {
-
+                this.directoryPath = response[0].path.substring(0, response[0].path.lastIndexOf('/'))
             }
         })
     }
