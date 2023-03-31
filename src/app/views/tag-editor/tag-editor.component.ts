02faa6655c0f59b039bdbf5fd54cc45bb2b15d25
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2023-03-31 16:48:08 -0600

    Display list of audio files

--- src/app/views/tag-editor/tag-editor.component.ts
+++ src/app/views/tag-editor/tag-editor.component.ts
@@ -1,5 +1,7 @@
-import { IpcService } from './../../services/ipc.service';
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { AudioFile } from './../../models/audiofile';
+import { IpcService } from './../../services/ipc.service';
 
 @Component({
     selector: 'app-tag-editor',
@@ -7,19 +9,32 @@ import { Component } from '@angular/core';
     styleUrls: ['./tag-editor.component.scss']
 })
 export class TagEditorComponent {
-    public directoryPath?: string;
+    public directoryPath: string = '';
+    public dataSource: MatTableDataSource<AudioFile>;
+    public displayedColumns: string[] = ['name', 'title', 'artist', 'actions'];
 
     public constructor(
         private ipcService: IpcService,
-    ) { }
+    ) {
+        this.dataSource = new MatTableDataSource<AudioFile>([]);
+    }
 
     public openFolder() {
         this.ipcService.readAudioTagsFromFolder().then(response => {
-            console.log(response)
-            if (response) {
+            this.dataSource.data = [];
+            if (response && response.length > 0) {
+                console.log(JSON.stringify(response))
                 this.directoryPath = response[0].path.substring(0, response[0].path.lastIndexOf('/'))
+                this.dataSource.data = response;
             }
         })
     }
 
+    public editTags(audio: AudioFile) {
+
+    }
+
+    public saveTags(audio: AudioFile) {
+
+    }
 }
